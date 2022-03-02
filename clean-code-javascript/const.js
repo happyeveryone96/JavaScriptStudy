ba180ec3851a7511f89b7ee0f6a1e9dfcec71d81
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-03-02 21:04:33 +0900

    const

--- clean-code-javascript/const.js
+++ clean-code-javascript/const.js
@@ -1,8 +1,13 @@
 // const
-// 재선언 불가능 / 재할당 불가능
+// 1. 재선언 불가능
 const name = '이름3';
 const name = '이름3';
 const name = '이름3';
-
 
 console.log(name); // SyntaxError: Identifier 'name' has already been declared
+
+// 2. 재할당 불가능
+const name = '이름3';
+name = '이름2';
+
+console.log(name); // TypeError: Assignment to constant variable.
